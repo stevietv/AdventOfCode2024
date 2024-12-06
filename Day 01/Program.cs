b99728b297c17d85f568e3896db808a4a3e7fabc
Stevie Robinson <stevie.robinson@gmail.com>
2024-12-06 19:24:01 +0100

    Day 01

---
+++ Day 01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+const string filename = "list.txt";
+
+using var reader = new StreamReader(filename);
+
+List<int> leftList = [];
+List<int> rightList = [];
+
+while (reader.ReadLine() is { } line)
+{
+    var lineItems = Regex.Split(line, @"\s+");
+    leftList.Add(Int32.Parse(lineItems[0]));
+    rightList.Add(Int32.Parse(lineItems[1]));
+}
+
+leftList.Sort();
+rightList.Sort();
+
+var accumulator = leftList.Zip(rightList, (a, b) => Math.Abs(a - b)).Sum();
+
+Console.WriteLine(accumulator);
